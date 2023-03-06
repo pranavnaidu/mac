2cf912c846f1e3b6f6b102c8cd27e687780f57cf
pranavnaidu <pranavnaid@gmail.com>
2023-03-06 17:18:42 -0600

    Initial Commit

---
+++ hello world/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  hello world
+//
+//  Created by Pranav Naidu on 3/6/23.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // insert code here...
+    cout << "Hello world" << endl;
+    
+    }
